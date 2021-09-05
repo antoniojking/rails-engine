fac212b77bbbeca2aadf463ae396fda6c4e71466
Antonio King <antonio.j.king@gmail.com>
2021-09-05 15:30:24 -0600

    Add numericality validation to item and inv_item models

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,6 +2,6 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
 
-  validates :quantity, presence: true
-  validates :unit_price, presence: true
+  validates :quantity, presence: true, numericality: true
+  validates :unit_price, presence: true, numericality: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,5 +11,6 @@ RSpec.describe Item do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:unit_price) }
+    it { should validate_numericality_of(:unit_price) }
   end
 end
