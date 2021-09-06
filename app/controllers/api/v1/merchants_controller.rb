b26feb49fdb53fd2f889993a157eb55904aed4d4
Antonio King <antonio.j.king@gmail.com>
2021-09-06 16:56:06 -0600

    Refactor merchant index paginate, postman passing (6/7)

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    @merchants = Merchant.paginate(page: params[:page], per_page: 20)
+    @merchants = Merchant.paginate(page: params[:page], per_page: params[:per_page] || 20)
     render json: MerchantSerializer.new(@merchants)
+
+    # @page = params.fetch(:page, 0).to_i
+    # @merchants_per_page = params.fetch(:per_page, 20).to_i
+    # if @page > 0
+    #   @merchants = Merchant.offset(@page * @merchants_per_page).limit(@merchants_per_page)
+    # else
+    #   @merchants = Merchant.offset(0).limit(@merchants_per_page)
+    # end
+    # render json: MerchantSerializer.new(@merchants)
   end
 end
