7be5ad4145f188d548b3a9bb698730c1d058d0c8
Antonio King <antonio.j.king@gmail.com>
2021-09-06 18:53:49 -0600

    Add set_page private method to merch controller; postman pass (7/7)

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    @merchants = Merchant.paginate(page: params[:page], per_page: params[:per_page] || 20)
+    @merchants = Merchant.paginate(page: set_page, per_page: params[:per_page] || 20)
     render json: MerchantSerializer.new(@merchants)
 
     # @page = params.fetch(:page, 0).to_i
@@ -11,5 +11,14 @@ class Api::V1::MerchantsController < ApplicationController
     #   @merchants = Merchant.offset(0).limit(@merchants_per_page)
     # end
     # render json: MerchantSerializer.new(@merchants)
+  end
+
+  private
+  def set_page
+    if params[:page].present? && params[:page].to_i < 1
+      page = 1
+    else
+      page = params[:page]
+    end
   end
 end
