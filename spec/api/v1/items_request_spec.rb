c295619341cc1f74e94000cab60fb64b0bfb96cf
Antonio King <antonio.j.king@gmail.com>
2021-09-07 16:49:09 -0600

    Create api for items show; postman passing (3/3)

--- spec/api/v1/items_request_spec.rb
+++ spec/api/v1/items_request_spec.rb
@@ -32,4 +32,41 @@ RSpec.describe 'Items API' do
   end
 
   it 'does not have data for merchant'
+
+  it 'sends a single item' do
+    merchant = create(:merchant)
+    item1 = merchant.items.create(name: "This Thing", description: "aldjsflasjdfl.", unit_price: 10.99)
+
+    get "/api/v1/items/#{item1.id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data][:id]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Numeric)
+
+    expect(item[:data][:attributes]).to have_key(:merchant_id)
+    expect(item[:data][:attributes][:merchant_id]).to be_a(Numeric)
+  end
+
+  it 'does not have merchant info'
+
+  xit 'returns 404 message when input invalid, is this something that needs to be tested?' do
+    merchant = create(:merchant)
+    item1 = Item.create(name: "This Thing", description: "aldjsflasjdfl.", unit_price: 10.99, merchant_id: "10")
+
+    get "/api/v1/items/#{item1.id}"
+
+    expect(response.status).to eq(404)
+  end
 end
